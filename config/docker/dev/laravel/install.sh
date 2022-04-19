308f1f5c76ea0797d599d941602dbf99fcb4307f
Kamil Niemczycki <contact@kamilcraft.com>
2022-04-19 18:39:14 +0200

    Updated installer
    Added key generation and database migrations

--- config/docker/dev/laravel/install.sh
+++ config/docker/dev/laravel/install.sh
@@ -5,7 +5,14 @@ if [ ! -d "vendor" ] && [ -f "composer.json" ]; then
     echo "########################################"
     echo "# vendor directory not found...        #"
     echo "########################################"
-    composer install
+    composer install \
+    && php artisan key:generate
+    
+    if [ ! -f "database/database.sqlite" ]; then
+        touch database/database.sqlite
+    fi
+    
+    php artisan migrate --seed
 fi
 
 if [ ! -d "node_modules" ] && [ -f "package.json" ]; then
