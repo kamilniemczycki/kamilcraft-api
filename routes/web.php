4b5e3b81bcd775a99b3c4b548fc91d87c75ab483
Kamil Niemczycki <contact@kamilcraft.com>
2022-05-19 13:22:01 +0200

    Clearing your router of comments

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::name('admin.')->group(function () {
     Route::namespace('Dashboard')->middleware('auth')->group(function () {
@@ -50,6 +41,7 @@ Route::name('admin.')->group(function () {
                 ->name('destroy');
         });
     });
+
     Route::name('auth.')->namespace('Auth')->group(function () {
         Route::get('login', 'LoginController@login')
             ->name('login');
